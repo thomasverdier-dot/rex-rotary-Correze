b10c97c8e0a26206c386ec1940dad25b62f3da4d
thomasverdier-dot <thomas.verdier@rex-rotary.fr>
2026-09-10 16:23:04 +0200

    Activer interface V3 Carte et Secteurs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_CACHE = "rex-correze-app-v5";
+const APP_CACHE = "rex-correze-app-v6";
 const DATA_CACHE = "rex-correze-data-v1";
 const APP_SHELL = [
   "./",
@@ -6,7 +6,8 @@ const APP_SHELL = [
   "./manifest.webmanifest",
   "./icon.svg",
   "./admin-seller.js",
-  "./weekly-forecast.js"
+  "./weekly-forecast.js",
+  "./ui-v3.js"
 ];
 
 self.addEventListener("install", event=>{
@@ -27,8 +28,9 @@ async function injectModules(response){
     if(!type.includes('text/html')) return response;
     let html=await response.text();
     const tags=[];
-    if(!html.includes('admin-seller.js')) tags.push('<script src="./admin-seller.js?v=5"></script>');
-    if(!html.includes('weekly-forecast.js')) tags.push('<script src="./weekly-forecast.js?v=1"></script>');
+    if(!html.includes('admin-seller.js')) tags.push('<script src="./admin-seller.js?v=6"></script>');
+    if(!html.includes('weekly-forecast.js')) tags.push('<script src="./weekly-forecast.js?v=2"></script>');
+    if(!html.includes('ui-v3.js')) tags.push('<script src="./ui-v3.js?v=1"></script>');
     if(tags.length) html=html.replace('</body>',tags.join('')+'</body>');
     const headers=new Headers(response.headers);
     headers.delete('content-length');
