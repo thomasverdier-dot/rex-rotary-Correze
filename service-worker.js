0fa75d90f071effb585fab138e8a838bdfdd5e77
thomasverdier-dot <thomas.verdier@rex-rotary.fr>
2026-09-10 17:16:18 +0200

    Load software ticket module

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_CACHE = "rex-correze-app-v11";
+const APP_CACHE = "rex-correze-app-v12";
 const DATA_CACHE = "rex-correze-data-v1";
 const APP_SHELL = [
   "./",
@@ -9,7 +9,8 @@ const APP_SHELL = [
   "./weekly-forecast.js",
   "./ui-v3.js",
   "./map-companies.js",
-  "./prospection.js"
+  "./prospection.js",
+  "./tickets.js"
 ];
 
 self.addEventListener("install", event=>{
@@ -35,6 +36,7 @@ async function injectModules(response){
     if(!html.includes('ui-v3.js')) tags.push('<script src="./ui-v3.js?v=1"></script>');
     if(!html.includes('map-companies.js')) tags.push('<script src="./map-companies.js?v=2"></script>');
     if(!html.includes('prospection.js')) tags.push('<script src="./prospection.js?v=1"></script>');
+    if(!html.includes('tickets.js')) tags.push('<script src="./tickets.js?v=1"></script>');
     if(tags.length) html=html.replace('</body>',tags.join('')+'</body>');
     const headers=new Headers(response.headers);
     headers.delete('content-length');
